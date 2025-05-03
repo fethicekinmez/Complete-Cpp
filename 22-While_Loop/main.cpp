1b5025b37188d7937e4d7eaba91db8664eb7d1d5
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-03 15:46:36 +0300

    While Loop is used

---
+++ 22-While_Loop/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    int i = 1; // Initialize the loop variable
+
+    while(i <= 10){ // Loop condition
+        std::cout << "This is iteration number: " << i << std::endl; // Loop body
+        i++; // Increment the loop variable
+    }
+
+    return 0;
+}
