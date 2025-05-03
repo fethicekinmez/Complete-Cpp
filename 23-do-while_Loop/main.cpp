c2c90580b4a7e598537df82fb8ada9f6a5e69735
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-03 15:54:13 +0300

    Add main.cpp with a do-while loop implementation

---
+++ 23-do-while_Loop/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(){
+
+    int i = 1;
+    do{
+        std::cout << "Iteration number: " << i << std::endl;
+        i++;
+    }while(i <= 10);
+
+
+    return 0;
+}
