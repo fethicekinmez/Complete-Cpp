130572c06f4e54fafea51221c7cca031039c2832
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-03 15:00:09 +0300

    Arithmetic operators are added

---
+++ 11-Arithmetic_Operators/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    int a = 5;
+    int b = 2;
+
+    std::cout << "Addition: " << (a + b) << std::endl; // 7
+    std::cout << "Subtraction: " << (a - b) << std::endl; // 3
+    std::cout << "Multiplication: " << (a * b) << std::endl; // 10
+    std::cout << "Division: " << (a / b) << std::endl; // 2
+    std::cout << "Modulus: " << (a % b) << std::endl; // 1
+
+    return 0;
+}
