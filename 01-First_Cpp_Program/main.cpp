0ae17da3c16f63c646cec3ae92647a2e432e1e5e
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-05-03 14:22:47 +0300

    Adding first program

---
+++ 01-First_Cpp_Program/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// This is a simple C++ program that prints "Hello, World!" to the console.
+
+int main(){
+
+    cout << "Hello, World!" << endl; // Output "Hello, World!" to the console
+    
+    return 0;
+}
